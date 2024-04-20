b86552d097a6eaf6fcffae340ff7ec3d748a0f79
aintburak <aintburak@gmail.com>
2024-04-20 14:04:17 +0300

    Add credit.c

--- Week01-C/ProblemSet1/credit/credit.c
+++ Week01-C/ProblemSet1/credit/credit.c
@@ -1,65 +1,64 @@
 #include <stdio.h>
+#include <math.h>
 
-int everyOtherDigit(long int cardNumber);
+int getDigits(long int cardNumber);
 void luhnsAlgorithm(long int cardNumber);
 void printType(long int cardNumber);
-int getDigits(long int cardNumber);
 
 int main(void) {
-
-    // get credit card info
-    long int cardNumber = 4003600000000014; // 16-digit number
-    do
-    {
-        //printf("Card Number: ");
-        //scanf("%ld", &cardNumber);
-        int digits = getDigits(cardNumber);
-        printf("the number --> %ld \nsize --> %ld",cardNumber,sizeof(cardNumber));
-        printf("\nDigits -- > %d",digits);
-        luhnsAlgorithm(cardNumber);
-    }
-    while (cardNumber < 0);
-}
-
-int everyOtherDigit(long int cardNumber) {
-
+    long int cardNumber = 4003600000000014; // Example card number
+    printf("Card Number: %ld\n", cardNumber);
+    int digits = getDigits(cardNumber);
+    printf("Digits: %d\n", digits);
+    luhnsAlgorithm(cardNumber);
+    printType(cardNumber);
+    return 0;
 }
 
 int getDigits(long int cardNumber) {
     int count = 0;
-
-    while(cardNumber > 0) {
+    while (cardNumber > 0) {
         count++;
         cardNumber /= 10;
     }
     return count;
-
 }
-
 
 void luhnsAlgorithm(long int cardNumber) {
     int sum = 0;
-    int lastDigit = 0;
+    int digitCount = getDigits(cardNumber);
+    int doubleDigit;
 
-   for( int i = 0; cardNumber > 0; i++) {
-        lastDigit = cardNumber%10;
-        printf("\n\n-> %d\n",lastDigit);
-        if(i%2 != 0)  {
-            sum += lastDigit;
+    // Process every second digit from the end
+    for (int i = 0; i < digitCount; i++, cardNumber /= 10) {
+        int digit = cardNumber % 10;
+        if (i % 2 == 0) {
+            sum += digit; // Sum of digits not multiplied by 2
+        } else {
+            doubleDigit = 2 * digit;
+            sum += (doubleDigit > 9) ? (doubleDigit - 9) : doubleDigit;
         }
-
-        
-        
-        printf("lastDigit = %d ~~~ cardnumber: %ld ~~~ Sum = %d\n",lastDigit,cardNumber,sum);
-
-        cardNumber /= 10;
     }
-    
-}
 
+    printf("Total Sum (Luhn's Check): %d\n", sum);
+    if (sum % 10 == 0) {
+        printf("Card is valid according to Luhn's Algorithm\n");
+    } else {
+        printf("Card is invalid\n");
+    }
+}
 
 void printType(long int cardNumber) {
-
+    int digits = getDigits(cardNumber);
+    int firstTwoDigits = (cardNumber / (long int)pow(10, digits - 2));
+    
+    if (digits == 15 && (firstTwoDigits == 34 || firstTwoDigits == 37)) {
+        printf("Type: American Express\n");
+    } else if (digits == 16 && (firstTwoDigits >= 51 && firstTwoDigits <= 55)) {
+        printf("Type: MasterCard\n");
+    } else if ((digits == 13 || digits == 16) && (firstTwoDigits / 10 == 4)) {
+        printf("Type: Visa\n");
+    } else {
+        printf("Unknown card type\n");
+    }
 }
-
-
