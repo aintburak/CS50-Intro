0e100e225d7dce63ad88042527b3f0f9d97444fc
aintburak <aintburak@gmail.com>
2024-04-25 14:24:41 +0300

    Week03: bubble sort methods template generated for .c file

--- Week03-Algorithms/BubbleSort/bubble.c
+++ Week03-Algorithms/BubbleSort/bubble.c
@@ -2,7 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void displayArray(int* array, int size);
+
+
 int main() {
 
+    // int* myRandomArray = {9,7,5,3,1,2,7,12}; in C, we , unfortunately, cannot init the array like this;
+    // But instead: Correctly initializing an array with a specific set of values
+    int myArray[] = {9, 7, 5, 3, 1, 2, 7, 12};
+    int mySize = sizeof(myArray) / sizeof(myArray[0]);
+    printf("The size of the array is: %d\n", size);
+    displayArray(myArray,mySize);
+    
+
     return 0;
+}
+
+
+void displayArray(int* array, int size){
+    
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
