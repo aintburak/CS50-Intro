2fc0bf95c70728f19d90063cbcde04be082be652
aintburak <aintburak@gmail.com>
2024-05-04 10:26:48 +0300

    Week04: update volume.c

--- Week04-Memory/ProblemSet4/Volume/volume.c
+++ Week04-Memory/ProblemSet4/Volume/volume.c
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
 
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    FILE *input = fopen(argv[1], "r"); // open input file as read mode
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    FILE *output = fopen(argv[2], "w"); // open output file as write mode
     if (output == NULL)
     {
         printf("Could not open file.\n");
