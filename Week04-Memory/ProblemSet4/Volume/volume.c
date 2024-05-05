d79edfc8db83edd3f2ee829122d12094930c1ae7
aintburak <aintburak@gmail.com>
2024-05-05 21:35:56 +0300

    Week04: Copy header from input file to output file

--- Week04-Memory/ProblemSet4/Volume/volume.c
+++ Week04-Memory/ProblemSet4/Volume/volume.c
@@ -36,6 +36,10 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
+    // Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output
    // Create a buffer for a single sample
     int16_t buffer;
 
@@ -51,6 +55,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
 
+    
     // Close files
     fclose(input);
     fclose(output);
