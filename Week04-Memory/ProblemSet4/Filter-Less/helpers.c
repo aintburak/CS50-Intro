0e856d20febeb35a47274d03e16d9f4afb7d7529
aintburak <aintburak@gmail.com>
2024-05-06 15:07:26 +0300

    Week04: Add Blur image template

--- Week04-Memory/ProblemSet4/Filter-Less/helpers.c
+++ Week04-Memory/ProblemSet4/Filter-Less/helpers.c
@@ -66,9 +66,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
 }
-
 
 void swap(RGBTRIPLE* a, RGBTRIPLE* b) {
     RGBTRIPLE temp = *a;
