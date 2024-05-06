6c01e32124b1abb695395a74f3b4ac3957f94451
aintburak <aintburak@gmail.com>
2024-05-06 18:18:14 +0300

    Week04: complete recover

--- Week04-Memory/ProblemSet4/recover/recover.c
+++ Week04-Memory/ProblemSet4/recover/recover.c
@@ -1,13 +1,59 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
 
-    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL) {
+        printf("Could not open the file.\n");
+        return 1;
+    }
 
-    // While there's still data left to read from the memory card
+    uint8_t buffer[512]; // unsigned integer 8-bytes as type
+    FILE *img = NULL;
+    char filename[8];
+    int filecount = 0;
+    
+    while (fread(buffer, 1, 512, card) == 512) // we are reading 512 bytes by 1 byte
+    {
+        // Check for JPEG header
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // Close the previous JPEG file if it exists
+            if (img != NULL)
+            {
+                fclose(img);
+            }
 
-        // Create JPEGs from the data
+            // Create a new JPEG file
+            sprintf(filename, "%03d.jpg", filecount++);
+            img = fopen(filename, "w");
+            if (img == NULL) {
+                printf("Could not create %s.\n", filename);
+                return 1;
+            }
+        }
+
+        // Write to the JPEG file if it is open
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+
+    // Close any remaining open files
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+
+    fclose(card);
+    return 0;
 }
