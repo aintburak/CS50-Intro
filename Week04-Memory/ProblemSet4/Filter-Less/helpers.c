637a80880e40400b850d83eb748df87151fbb3d3
aintburak <aintburak@gmail.com>
2024-05-06 14:33:30 +0300

    week04: complete sepia

--- Week04-Memory/ProblemSet4/Filter-Less/helpers.c
+++ Week04-Memory/ProblemSet4/Filter-Less/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
@@ -8,9 +9,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
         for (int j = 0; j < width; j++) {
             // Take average of red, green, and blue
 
-            image[i][j].rgby
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
 
             // Update pixel values
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
         }
     }
 }
@@ -18,7 +22,30 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Compute sepia values
+
+            float originalBlue= (float)image[i][j].rgbtBlue;
+            float originalGreen = (float)image[i][j].rgbtGreen;
+            float originalRed = (float)image[i][j].rgbtRed;
+
+            // ternary op: c = (a < b) ? a : b;
+
+            int sepiaRed =  (round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue) > 255) ? 255 : round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = (round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue) > 255) ? 255 : round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = (round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue) > 255) ? 255 : round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            // Update pixel with sepia values
+
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
 }
 
 // Reflect image horizontally
