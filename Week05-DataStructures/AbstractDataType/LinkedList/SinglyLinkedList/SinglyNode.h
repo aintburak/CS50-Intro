46d6ad431e7359b99f62e39fdfbd3ed0734126a3
aintburak <aintburak@gmail.com>
2024-05-09 15:10:32 +0300

    Week05: add singly node

---
+++ Week05-DataStructures/AbstractDataType/LinkedList/SinglyLinkedList/SinglyNode.h
@@ -0,0 +1,15 @@
+#ifndef SINGLYNODE_H
+#define SINGLYNODE_H
+
+typedef struct SinglyNode
+{
+    /* data */
+    int value;
+    struct SinglyNode* next;
+
+};
+SinglyNode;
+
+
+
+#endif // end SINGLYNODE_H
