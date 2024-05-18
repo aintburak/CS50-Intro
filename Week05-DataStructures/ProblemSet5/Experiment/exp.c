7840a1f8b70cdf2733bbcee8a90768bfc6705e95
aintburak <aintburak@gmail.com>
2024-05-18 16:50:02 +0300

    Week05: checkpoint v2

--- Week05-DataStructures/ProblemSet5/Experiment/exp.c
+++ Week05-DataStructures/ProblemSet5/Experiment/exp.c
@@ -2,16 +2,24 @@
 #include<stdbool.h>
 #include<stdio.h>
 
+#define N 26
+
 int main() {
     char c;
+    char word[N];
+    int i = 0;
     char* filename = "text.txt";
     FILE *file = fopen(filename, "r");
     if (file != NULL) {
         while ((c = fgetc(file)) != EOF) {
             // Process the character read
-            printf("%c",c);
-            if(c == '\0')
-                printf(" -- ");
+            //printf("%c",c);
+            word[i] = c;
+            if(c == ' ') {
+                printf("\n --> %s\n",word);
+                break;
+            }
+            i++;
         }
         fclose(file);
     } else {
