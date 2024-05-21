9abb7f4d15a98a0c913e3935504672be936df539
aintburak <aintburak@gmail.com>
2024-05-21 23:04:50 +0300

    Week06: generate main metod inside python

--- Week06-Python/ProblemSet6/Sentiment-Mario-Less/mario.py
+++ Week06-Python/ProblemSet6/Sentiment-Mario-Less/mario.py
@@ -1,6 +1,16 @@
 def main():
-  # Your program's core logic here
-  print("Hello, world!")
+    # Prompt the user for the height of the pyramid
+    while True:
+        height = input("Height: ")
+        if height.isdigit():
+            height = int(height)
+            if 0 < height <= 8:
+                break
 
-if __name__ == '__main__':
-  main()
+    # Print the pyramid
+    for i in range(1, height + 1):
+        print(" " * (height - i) + "#" * i)
+
+
+if __name__ == "__main__":
+    main()
