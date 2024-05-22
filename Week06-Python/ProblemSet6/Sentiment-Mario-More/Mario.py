7463fa20f4e1d158c22a62f8ccc03d0185197594
aintburak <aintburak@gmail.com>
2024-05-22 22:04:08 +0300

    Week06: complete Mario More py :D

---
+++ Week06-Python/ProblemSet6/Sentiment-Mario-More/Mario.py
@@ -0,0 +1,16 @@
+def main():
+    # Prompt the user for the height of the pyramid
+    while True:
+        height = input("Height: ")
+        if height.isdigit():
+            height = int(height)
+            if 0 < height <= 8:
+                break
+
+    # Print the pyramid
+    for i in range(1, height + 1):
+        print(" " * (height - i) + "#" * i + "  " + (i) * "#")
+
+
+if __name__ == "__main__":
+    main()
