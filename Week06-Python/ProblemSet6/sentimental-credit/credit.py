260b4d59e8be9c8618a1149d3e49541d58ea286e
aintburak <aintburak@gmail.com>
2024-05-23 12:47:59 +0300

    Week06: add more code

--- Week06-Python/ProblemSet6/sentimental-credit/credit.py
+++ Week06-Python/ProblemSet6/sentimental-credit/credit.py
@@ -14,16 +14,57 @@ def returnEveryOtherDigit(cardNumber):
     # Use slicing to get every other digit
     return str(cardNumber)[-2::-2]
 
+def returnRemainingDigit(cardNumber):
+    # Use slicing to get every other digit
+    return str(cardNumber)[-1::-2]
+
+def sumDigits(number):
+    total_sum = 0
+    # Iterate over each digit of the number
+    for digit_char in str(number):
+        # Convert the digit character to an integer and add it to the total sum
+        total_sum += int(digit_char)
+    
+    return total_sum
+
 
 def luhnsAlgorithm(cardNumber):
     # Luhn's Algorithm
-    everyOtherDigit = returnEveryOtherDigit(cardNumber)
-    print(everyOtherDigit)
+    sum = 0
+    everyOtherDigit = str(returnEveryOtherDigit(cardNumber))
+    for i in everyOtherDigit:
+        sum+= sumDigits(2*int(i))
+        print(f"numb: {2*int(i)} numb[:]-> {str(2*int(i))[::1]} sum -> {sum}")
+    
+    remainingDigit = returnRemainingDigit(cardNumber)
+    
+    for j in remainingDigit:
+        sum+= int(j)
+    
+    print(f"sum: {sum}")
+
+    if sum % 10 == 0:
+        return True
+    else:
+        return False
     
 
 def main():
-    cardNumber = promtUser()
-    luhnsAlgorithm(cardNumber)
+    cardNumber = "4003600000000014"
+    print(luhnsAlgorithm(cardNumber))
+    
+
+    if luhnsAlgorithm(cardNumber):
+        if (cardNumber[:2] in {'51', '52', '53', '54', '55'}) and (len(cardNumber) == 16):
+            print("MASTERCARD")
+        elif (cardNumber[:2] in {'34', '37'}) and (len(cardNumber) == 15):
+            print("AMEX")
+        elif (cardNumber[:1] in {'4'}) and (str(len(cardNumber)) in {'13', '16'}):
+            print("VISA")
+        else:
+            print("INVALID")
+    else:
+        print("INVALID")
 
 
 
