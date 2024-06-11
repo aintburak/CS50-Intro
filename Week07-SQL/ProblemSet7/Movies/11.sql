3eedb0b183064256704dfd0d1d412851e58dd3a2
catnys <catnyseverdyn@gmail.com>
2024-06-11 09:56:38 +0300

    Week07: Fix sql 11

--- Week07-SQL/ProblemSet7/Movies/11.sql
+++ Week07-SQL/ProblemSet7/Movies/11.sql
@@ -1,8 +1,8 @@
-SELECT m.movies, m.title
+SELECT m.title
 FROM movies AS m
 JOIN stars AS s ON s.movie_id = m.id
 JOIN people AS p ON p.id = s.person_id
-JOIN ratings AS r ON r.movie_id ? m.id
+JOIN ratings AS r ON r.movie_id = m.id
 WHERE p.name = 'Chadwick Boseman'
 ORDER BY r.rating ASC
 LIMIT 5;
