55ba59ee7c6f4bd95a8b633cf05404905315fdfb
catnys <catnyseverdyn@gmail.com>
2024-06-11 12:49:13 +0300

    Week07: Fiftyville mystery solved :)

--- Week07-SQL/ProblemSet7/Fiftyville/log.sql
+++ Week07-SQL/ProblemSet7/Fiftyville/log.sql
@@ -295,7 +295,7 @@ ORDER BY hour ASC;
 */
 
 
-
+/*
 SELECT * FROM people LIMIT 5;
 
 
@@ -316,8 +316,8 @@ JOIN bank_accounts AS ba ON ba.account_number = atm.account_number
 JOIN people as ppl ON ppl.id = ba.person_id
 WHERE atm.atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
 AND day = 28 AND month = 7
-AND ppl.license_plate IN ( SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 )
-AND ppl.phone_number IN ( SELECT pc.caller FROM phone_calls AS pc WHERE pc.duration <= 60 AND day = 28 AND month = 7 )
+AND ppl.license_plate IN ( SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND hour = 10 AND minute BETWEEN 5 AND 25  )
+AND ppl.phone_number IN ( SELECT pc.caller FROM phone_calls AS pc WHERE pc.duration < 60 AND day = 28 AND month = 7 )
 AND ppl.passport_number IN
 (SELECT p.passport_number FROM airports as air
 JOIN flights AS f ON f.origin_airport_id = air.id
@@ -325,3 +325,79 @@ JOIN passengers AS p ON  p.flight_id = f.id
 WHERE air.id = 8 AND f.id = 36
 AND day = 29 AND month = 7
 ORDER BY hour ASC);
+*/
+
+
+/*
+
+
+SELECT * FROM atm_transactions AS atm
+JOIN bank_accounts AS ba ON ba.account_number = atm.account_number
+JOIN people as ppl ON ppl.id = ba.person_id
+WHERE atm.atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
+AND day = 28 AND month = 7
+AND ppl.license_plate IN ( SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND hour = 10 AND minute BETWEEN 5 AND 25  )
+AND ppl.phone_number IN ( SELECT pc.caller FROM phone_calls AS pc WHERE pc.duration < 60 AND day = 28 AND month = 7 )
+AND ppl.passport_number IN
+(SELECT p.passport_number FROM airports as air
+JOIN flights AS f ON f.origin_airport_id = air.id
+JOIN passengers AS p ON  p.flight_id = f.id
+WHERE air.id = 8 AND f.id = 36
+AND day = 29 AND month = 7
+ORDER BY hour ASC);
+
++-----+----------------+------+-------+-----+----------------+------------------+--------+----------------+-----------+---------------+--------+-------+----------------+-----------------+---------------+
+| id  | account_number | year | month | day |  atm_location  | transaction_type | amount | account_number | person_id | creation_year |   id   | name  |  phone_number  | passport_number | license_plate |
++-----+----------------+------+-------+-----+----------------+------------------+--------+----------------+-----------+---------------+--------+-------+----------------+-----------------+---------------+
+| 267 | 49610011       | 2023 | 7     | 28  | Leggett Street | withdraw         | 50     | 49610011       | 686048    | 2010          | 686048 | Bruce | (367) 555-5533 | 5773159633      | 94KL13X       |
++-----+----------------+------+-------+-----+----------------+------------------+--------+----------------+-----------+---------------+--------+-------+----------------+-----------------+---------------+
+
+
+*/
+
+
+
+----------------------------------------
+----------- CHECKPOINT V1.5 ------------
+----------------------------------------
+/*
+    Now, We know bruce has stolen the CS50 Duck, it is time to find his alliance :)
+
+*/
+
+SELECT * FROM people;
+.schema people
+
+
+SELECT * FROM phone_calls LIMIT 5;
+.schema phone_calls
+
+
+
+SELECT * FROM people AS ppl
+JOIN phone_calls AS pc ON pc.caller = ppl.phone_number
+WHERE pc.duration <= 60
+AND day = 28 AND month = 7;
+
+
+/*
+
++--------+---------+----------------+-----------------+---------------+-----+----------------+----------------+------+-------+-----+----------+
+|   id   |  name   |  phone_number  | passport_number | license_plate | id  |     caller     |    receiver    | year | month | day | duration |
++--------+---------+----------------+-----------------+---------------+-----+----------------+----------------+------+-------+-----+----------+
+| 398010 | Sofia   | (130) 555-0289 | 1695452385      | G412CB7       | 221 | (130) 555-0289 | (996) 555-8899 | 2023 | 7     | 28  | 51       |
+| 560886 | Kelsey  | (499) 555-9472 | 8294398571      | 0NTHK55       | 224 | (499) 555-9472 | (892) 555-8872 | 2023 | 7     | 28  | 36       |
+| 686048 | Bruce   | (367) 555-5533 | 5773159633      | 94KL13X       | 233 | (367) 555-5533 | (375) 555-8161 | 2023 | 7     | 28  | 45       |
+| 561160 | Kathryn | (609) 555-5876 | 6121106406      | 4ZY7I8T       | 234 | (609) 555-5876 | (389) 555-5198 | 2023 | 7     | 28  | 60       |
+| 560886 | Kelsey  | (499) 555-9472 | 8294398571      | 0NTHK55       | 251 | (499) 555-9472 | (717) 555-1342 | 2023 | 7     | 28  | 50       |
+| 449774 | Taylor  | (286) 555-6063 | 1988161715      | 1106N58       | 254 | (286) 555-6063 | (676) 555-6554 | 2023 | 7     | 28  | 43       |
+| 514354 | Diana   | (770) 555-1861 | 3592750733      | 322W7JE       | 255 | (770) 555-1861 | (725) 555-3243 | 2023 | 7     | 28  | 49       |
+| 907148 | Carina  | (031) 555-6622 | 9628244268      | Q12B3Z3       | 261 | (031) 555-6622 | (910) 555-3251 | 2023 | 7     | 28  | 38       |
+| 395717 | Kenny   | (826) 555-1652 | 9878712108      | 30G67EN       | 279 | (826) 555-1652 | (066) 555-9701 | 2023 | 7     | 28  | 55       |
+| 438727 | Benista | (338) 555-6650 | 9586786673      | 8X428L0       | 281 | (338) 555-6650 | (704) 555-2131 | 2023 | 7     | 28  | 54       |
+
+
+in the people table, it can be found that
+
+| 864400 | Robin       | (375) 555-8161 | NULL            | 4V16VO0       |
+*/
